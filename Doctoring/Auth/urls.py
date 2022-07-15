6d4a405be94ba6a322e3fdb32b19942191ae8629
Neeraj319 <itskop520@gmail.com>
2022-07-15 18:49:44 +0545

    [added] receptionist signup

--- Doctoring/Auth/urls.py
+++ Doctoring/Auth/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_user, signup_doctor, signup
+from .views import login_user, signup_doctor, signup, signup_receptionist
 
 urlpatterns = [
     path("login/", login_user, name="login"),
     path("signup/", signup, name="signup"),
     path("signup/doctor/", signup_doctor, name="signup_doctor"),
+    path("signup/receptionist/", signup_receptionist, name="signup_receptionist"),
 ]
--- Doctoring/Auth/views.py
+++ Doctoring/Auth/views.py
@@ -10,8 +10,8 @@ def login_user(request: HttpRequest):
     if not request.user.is_authenticated:
 
         if request.method == "POST":
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST.get("username")
+            password = request.POST.get("password")
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
@@ -32,50 +32,112 @@ def signup(request: HttpRequest):
         return redirect("dashboard")
 
 
+def validate_credentials(username, password, confirm_password, phone):
+    if User.objects.filter(username=username).first():
+        return "Username already exists"
+    if password != confirm_password:
+        return "Passwords do not match"
+    if len(password) < 8:
+        return "Password must be at least 8 characters"
+    if len(phone) > 15:
+        return "Phone number must be smaller than 15 digits"
+
+
 def signup_doctor(request: HttpRequest):
     if not request.user.is_authenticated:
 
         if request.method == "POST":
-            username = request.POST["username"]
-            password = request.POST["password"]
-            confirm_password = request.POST["confirm_password"]
-            first_name = request.POST["first_name"]
-            last_name = request.POST["last_name"]
-            email = request.POST["email"]
-            phone = request.POST["phone"]
-            qualification = request.POST["qualification"]
-            years_practiced = request.POST["years_practiced"]
-            gender = request.POST.get("gender")
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            confirm_password = request.POST.get("confirm_password")
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            email = request.POST.get("email")
+            phone = request.POST.get("phone")
+            qualification = request.POST.get("qualification")
+            years_practiced = request.POST.get("years_practiced")
+            gender = request.POST.get.get("gender")
 
-            if User.objects.filter(username=username).first():
-                messages.error(request, "Username already exists")
+            if message := validate_credentials(
+                username=username,
+                password=password,
+                confirm_password=confirm_password,
+                phone=phone,
+            ):
+                messages.error(request, message)
                 return redirect("signup")
-            else:
-                if password != confirm_password:
-                    messages.error(request, "Passwords do not match")
-                    return render(request, "signup.html")
-                if len(password) < 8:
-                    messages.error(request, "Password must be at least 8 characters")
-                    return render(request, "signup.html")
-                user = User.objects.create_user(
-                    username=username,
-                    password=password,
-                    first_name=first_name,
-                    last_name=last_name,
-                    email=email,
-                )
-                Doctor.objects.create(
-                    user=user,
-                    phone=phone,
-                    qualification=qualification,
-                    years_practiced=years_practiced,
-                    gender=gender,
-                )
-                messages.success(
-                    request,
-                    "Your account has been created successfully",
-                )
-                return redirect("login")
+
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+            )
+            Doctor.objects.create(
+                user=user,
+                phone=phone,
+                qualification=qualification,
+                years_practiced=years_practiced,
+                gender=gender,
+            )
+            messages.success(
+                request,
+                "Your account has been created successfully",
+            )
+            return redirect("login")
+
+        return redirect("login")
+    else:
+        return redirect("dashboard")
+
+
+def signup_receptionist(request: HttpRequest):
+    if not request.user.is_authenticated:
+
+        if request.method == "POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            confirm_password = request.POST.get("confirm_password")
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            email = request.POST.get("email")
+            phone = request.POST.get("phone")
+            print(
+                username,
+                password,
+                confirm_password,
+                first_name,
+                last_name,
+                email,
+                phone,
+            )
+
+            if message := validate_credentials(
+                username=username,
+                password=password,
+                confirm_password=confirm_password,
+                phone=phone,
+            ):
+                messages.error(request, message)
+                return redirect("signup")
+
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+            )
+            Receptionist.objects.create(
+                user=user,
+                phone=phone,
+            )
+            messages.success(
+                request,
+                "Your account has been created successfully",
+            )
+            return redirect("login")
 
         return redirect("login")
     else:
