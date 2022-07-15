823bbbf3b14619625c60f8b7b9a5d7d315b4448f
Neeraj319 <itskop520@gmail.com>
2022-07-15 09:21:35 +0545

    [fixed] to a single db connection

--- Database/db_init.py
+++ Database/db_init.py
@@ -13,89 +13,88 @@ dotenv.load_dotenv()
 engine = sqlalchemy.create_engine(os.environ.get("DB_URL"))
 
 
-def create_tables():
+def create_tables(connection):
     with open("schema.sql", "r") as f:
         schema = f.read()
-        with engine.connect() as connection:
-            connection.execute(statement=schema)
+
+        connection.execute(statement=schema)
 
 
-def insert_manager():
-    with engine.connect() as connection:
-        for _ in range(5):
-            query = f"""
-            INSERT INTO "Manager" (name) values (%s);
-            """
-            connection.execute(query, (faker.name(),))
+def insert_manager(connection):
+
+    for _ in range(5):
+        query = f"""
+        INSERT INTO "Manager" (name) values (%s);
+        """
+        connection.execute(query, (faker.name(),))
 
 
-def insert_parent_department():
+def insert_parent_department(connection):
 
     query = f"""
     INSERT INTO "ParentDepartment" (name) values ('General Management');
     """
-    with engine.connect() as connection:
-        connection.execute(query)
+
+    connection.execute(query)
 
 
-def get_managers():
+def get_managers(connection):
 
     query = f"""
     SELECT * FROM "Manager";
     """
-    with engine.connect() as connection:
-        return connection.execute(statement=query)
+
+    return connection.execute(statement=query)
 
 
-def insert_departments():
+def insert_departments(connection):
     departments = ["Human Resources", "IT", "Marketing", "Sales"]
-    with engine.connect() as connection:
-        for department, manager in zip(departments, get_managers()):
-            query = f"""
-            INSERT INTO "Department" (dept_name, parent_dept_id, manager_id) values (%s, %s, %s);
-            """
-            connection.execute(query, (department, 1, manager[0]))
+
+    for department, manager in zip(departments, get_managers(connection=connection)):
+        query = f"""
+        INSERT INTO "Department" (dept_name, parent_dept_id, manager_id) values (%s, %s, %s);
+        """
+        connection.execute(query, (department, 1, manager[0]))
 
 
-def get_departments():
+def get_departments(connection):
 
     query = f"""
     SELECT * FROM "Department";
     """
-    with engine.connect() as connection:
-        return connection.execute(query)
+    return connection.execute(query)
 
 
-def insert_employees():
+def insert_employees(connection):
 
-    departments = list(get_departments())
-    with engine.connect() as connection:
-        for _ in range(10):
-            joined_date = faker.date_between(start_date="-5y", end_date="today")
-            monthly_salary = random.randint(5000, 10000)
-            query = f"""
-            INSERT INTO "Employee" (first_name, last_name, join_date, monthly_salary, dept_id) values (%s, %s, %s, %s, %s);
-            """
-            connection.execute(
-                query,
-                (
-                    faker.first_name(),
-                    faker.last_name(),
-                    joined_date,
-                    monthly_salary,
-                    random.choice(departments)[0],
-                ),
-            )
+    departments = list(get_departments(connection=connection))
+    for _ in range(10):
+        joined_date = faker.date_between(start_date="-5y", end_date="today")
+        monthly_salary = random.randint(5000, 10000)
+        query = f"""
+        INSERT INTO "Employee" (first_name, last_name, join_date, monthly_salary, dept_id) values (%s, %s, %s, %s, %s);
+        """
+        connection.execute(
+            query,
+            (
+                faker.first_name(),
+                faker.last_name(),
+                joined_date,
+                monthly_salary,
+                random.choice(departments)[0],
+            ),
+        )
 
 
 def main():
     if command == "initialize":
+        with engine.connect() as connection:
 
-        create_tables()
-        insert_manager()
-        insert_parent_department()
-        insert_departments()
-        insert_employees()
+            create_tables(connection=connection)
+            insert_manager(connection=connection)
+            insert_parent_department(connection=connection)
+            insert_departments(connection=connection)
+            insert_employees(connection=connection)
 
         print("initialization complete")
 
